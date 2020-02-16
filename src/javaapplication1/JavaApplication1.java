13a769a6879e1a20ca5c258d2bc38a7da2c09b7e
Luckae <lawsonluke9@gmail.com>
2020-02-16 02:47:34 +0100

    First Commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author mac
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+        
+        This is a comment
+        Another comment line here
+        */
+        
+//        System.out.println("Hello World!");
+//        System.out.println("Another Program here");
+        
+        byte bt;
+        bt = 127;
+        
+        int a = 23;
+        short sh = 344;
+        long lg = 233333;
+        float fl = 2.333f;
+        double db = 3.4444;
+        char ch = 'a';
+        boolean bl = true;
+        
+        int age = 23;
+        
+       // System.out.println(bt);
+        System.out.println(a);
+        
+        System.out.println(sh);
+        
+        String str = "My name is Lawson Luke";
+        
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        
+        
+        System.out.println("Lawson is"+" "+age + " " + "years old.");
+        
+        /*
+        
+        Concantenation: join
+        */
+        
+        
+    }
+    
+}
